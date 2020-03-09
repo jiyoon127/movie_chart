516fcaceeefb7218a8d95208c0f95248bb5810f7
jiyoon127 <38906480+jiyoon127@users.noreply.github.com>
2020-03-09 18:16:31 +0900

    Updates

---
+++ precache-manifest.226a30efbd4195b04e8f6baa5d2862c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddfa59bd66064f9f8925d9cef8c23daf",
+    "url": "/movie_chart/index.html"
+  },
+  {
+    "revision": "d1fb0393457607d64675",
+    "url": "/movie_chart/static/css/main.621e49b6.chunk.css"
+  },
+  {
+    "revision": "5eee216a114636080939",
+    "url": "/movie_chart/static/js/2.fa624c05.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_chart/static/js/2.fa624c05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1fb0393457607d64675",
+    "url": "/movie_chart/static/js/main.9a3bf9ee.chunk.js"
+  },
+  {
+    "revision": "4754cd871b79516b512c",
+    "url": "/movie_chart/static/js/runtime-main.5eeddf70.js"
+  }
+]);
