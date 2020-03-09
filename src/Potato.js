c0b9f183e729949eb40ae7b08c3b87b46bbc1480
jiyoon127 <38906480+jiyoon127@users.noreply.github.com>
2020-03-09 16:09:15 +0900

    movie component
    creating movie component and fecthing movie datas from api using axios

---
+++ src/Potato.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Potato extends Component {
+  render() {
+    return (
+      <div>
+        <h3> I love POTATO </h3>
+      </div>
+    );
+  }
+}
+
+export default Potato;
