8c2254781244e898d2f995c5a4f3b64480e05ce3
jiyoon127 <38906480+jiyoon127@users.noreply.github.com>
2020-03-10 19:12:55 +0900

    add router and navigation
    + css settings

---
+++ src/routes/Detail.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import './Detail.css';
+import axios from 'axios';
+
+class Detail extends Component {
+  state = {
+    movie: [],
+  };
+
+  getDetails = async id => {
+    console.log({ id });
+    if (id) {
+      const {
+        data: {
+          data: { movie },
+        },
+      } = await axios.get(
+        `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`,
+      );
+
+      this.setState({ movie });
+    }
+  };
+
+  componentDidMount() {
+    console.log(this.props);
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push('/');
+    }
+    this.getDetails(location.state.id);
+  }
+
+  render() {
+    const { location } = this.props;
+    const { movie } = this.state;
+    console.log({ movie });
+    if (movie) {
+      return (
+        <div className="movie__detail">
+          <div className="center">
+            <div
+              className="img"
+              style={{ backgroundImage: `url(${movie.large_cover_image})` }}
+            />
+          </div>
+
+          <div className="movie__descriptions">
+            <p className="movie__title">{movie.title}</p>
+            <div className="movie__sub">
+              <p style={{ float: 'left' }} className="movie__year">
+                {movie.year}
+              </p>
+              <ul className="movie__genres">
+                {location.state.genres.map((genre, index) => (
+                  <li key={index} className="genres__genre">
+                    {genre}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="info">
+              <div
+                style={{ color: 'grey', fontFamily: 'sans-serif' }}
+                className="info__title"
+              >
+                <p>Rating</p>
+                <p>Language</p>
+                <p>Length</p>
+              </div>
+              <div className="info__content">
+                <p>{movie.rating}</p>
+                <p>{movie.language}</p>
+                <p>{movie.runtime}min</p>
+              </div>
+            </div>
+            <div className="synopsis">
+              <p style={{ fontWeight: 800 }}>Synopsis</p>
+              <p className="movie__summary">{movie.description_full}</p>
+            </div>
+          </div>
+        </div>
+      );
+    } else return null;
+  }
+}
+
+export default Detail;
